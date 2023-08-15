cdd5481275bb09117dd4cbe35d170db42145e116
jhchenjh <chenjh1997@yahoo.com>
2023-08-16 00:04:38 +0800

    update codebase

--- main.py
+++ main.py
@@ -1,99 +1,70 @@
+import argparse
 import os
 import yaml
-import torch
-import argparse
-import logging
 import shutil
-import numpy as np
 import random
 from easydict import EasyDict
-import spring.linklink as link
+import torch
+import torch.distributed as dist
 
-from model.trainers import *
-from model.data import get_dataset_entry
-from model.utils.distributed_utils import dist_init
+from model.trainers.hf_trainer import Trainer
 
-def set_random_seed(seed=0):
+
+parser = argparse.ArgumentParser(description='PyTorch HierarchyFlow Training')
+parser.add_argument('--config', type=str, default='configs/config.yaml', help='config file')
+parser.add_argument('--eval_only', action='store_true', help='evaluation mode')
+parser.add_argument('--local_rank', type=int, default=-1, help='node rank for distributed training')
+parser.add_argument('--seed', type=int, default=0, help='seed for initializing training')
+
+
+def set_random_seed(seed):
+    r"""Set random seeds for everything.
+
+    Args:
+        seed (int): Random seed.
+    """
     random.seed(seed)
-    np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
 
-def main(args):
-    rank, world_size = dist_init()
-    with open(args.config) as f:
-        config = yaml.load(f,Loader=yaml.FullLoader)
-
-    config = EasyDict(config['common'])
-    name = args.config.split('/')[-1].split('.')[0]
-    config.save_path = os.path.join(args.save_path,name)
-    config.load_path = args.load_path
-    config.evaluate = args.evaluate
-    config.finetune = args.finetune
-
-
-    if rank == 0:
-        print('evaluate: ',config.evaluate)
-        if not os.path.exists(config.save_path):
-            os.makedirs(config.save_path)
-            print('mkdir save_path')
-
-        shutil.copyfile(args.config,os.path.join(config.save_path,'config.yaml'))
-        if not os.path.exists(os.path.join(config.save_path,'events')):
-            os.makedirs(os.path.join(config.save_path,'events'))
-            print('mkdir events_path')
-
-        if not os.path.exists(os.path.join(config.save_path,'logs')):
-            os.makedirs(os.path.join(config.save_path,'logs'))
-            print('mkdir logs_path')
-
-        if not os.path.exists(os.path.join(config.save_path,'ckpt')):
-            os.makedirs(os.path.join(config.save_path,'ckpt'))
-            print('mkdir ckpt_path')
-
-        if not os.path.exists(os.path.join(config.save_path,'imgs')):
-            os.makedirs(os.path.join(config.save_path,'imgs'))
-            print('mkdir imgs_path')
-            
-    if args.seed != -1:
-        config['seed'] = args.seed
-
-    set_random_seed(config['seed'])
-
-    dataset = get_dataset_entry(config)
-
-    if not config.evaluate:
-        trainer = globals()[config.trainer](config)
-        trainer._train_model(dataset,last_iter=-1)
-    else:
-        if not os.path.exists(os.path.join(config.save_path,args.pred_path,'pred')):
-            os.makedirs(os.path.join(config.save_path,args.pred_path,'pred'))
-            print('mkdir imgs_path')
-        if not os.path.exists(os.path.join(config.save_path,args.pred_path,'cat')):
-            os.makedirs(os.path.join(config.save_path,args.pred_path,'cat'))
-            print('mkdir imgs_path')
-        config.pred_path = args.pred_path
-
-        trainer = globals()[config.trainer](config)
-        trainer._eval_model(dataset,last_iter=-1)
-
-    return
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Hierarchy Flow')
-    parser.add_argument('--config', default='../config/cash/cash_osr.yaml')
-    parser.add_argument('--save_path', default='./')
-    parser.add_argument('--pred_path', default='./')
-    parser.add_argument('--load_path', default='', type=str)
-    parser.add_argument('--recover', action='store_true')
-    parser.add_argument('--seed', default=-1)
-    parser.add_argument('-e', '--evaluate', action='store_true')
-    parser.add_argument('--finetune', action='store_true')
-    parser.add_argument('--sync', action='store_true')
-    parser.add_argument('--fake', action='store_true')
-    parser.add_argument('--fuse-prob', action='store_true')
-    parser.add_argument('--fusion-list', nargs='+', help='multi model fusion list')
+def main():
     args = parser.parse_args()
-    main(args)
-    link.finalize()
+    args.nprocs = torch.cuda.device_count()
+    print(args.nprocs, args.local_rank)
+    set_random_seed(args.seed)
+
+    dist.init_process_group(backend='nccl')
+    torch.backends.cudnn.benchmark = True
+
+    with open(args.config) as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    cfg = EasyDict(cfg)
+    cfg.eval_mode = args.eval_only
+
+    if args.local_rank == 0:
+        print(cfg)
+        if not os.path.exists(cfg.output):
+            os.makedirs(cfg.output)
+        if not os.path.exists(os.path.join(cfg.output, cfg.task_name)):
+            os.makedirs(os.path.join(cfg.output, cfg.task_name))
+        if not os.path.exists(os.path.join(cfg.output, cfg.task_name, 'img_save')):
+            os.makedirs(os.path.join(cfg.output, cfg.task_name, 'img_save'))
+        if not os.path.exists(os.path.join(cfg.output, cfg.task_name, 'model_save')):
+            os.makedirs(os.path.join(cfg.output, cfg.task_name,'model_save'))
+        if not os.path.exists(os.path.join(cfg.output, cfg.task_name, 'eval_results')):
+            os.makedirs(os.path.join(cfg.output, cfg.task_name,'eval_results'))
+            os.makedirs(os.path.join(cfg.output, cfg.task_name,'eval_results', 'pred'))
+            os.makedirs(os.path.join(cfg.output, cfg.task_name,'eval_results', 'cat_img'))
+        shutil.copy(os.path.join(args.config), os.path.join(cfg.output, cfg.task_name, 'cfg.yaml'))
+
+    trainer = Trainer(cfg, args.local_rank, args.nprocs)
+
+    if args.eval_only:
+        trainer.eval()
+        return
+
+    trainer.train()
+
+
+if __name__ == "__main__":
+    main()
